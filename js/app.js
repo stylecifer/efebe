cd3f45cbd4b0f9323ee8a181a7c3d3d18fad6eab
VAntonIO <stylustonypro@gmail.com>
2017-08-03 03:03:54 -0400

    Agregué el plugin instafeed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 $(document).foundation()
+var feed = new Instafeed({
+    get: 'user',
+    userId: 'stylustonypro',
+    accessToken: '3751148176.8b0da27.ac48429dc25b4d2ba0afa2f171c493ef'
+
+    });
+    feed.run();
+
 // var icon = document.getElementById("icon");
 //
 // icon.onclick = function() {
